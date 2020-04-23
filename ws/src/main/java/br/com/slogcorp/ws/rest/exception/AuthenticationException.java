952cdb691adc9308cf136693020191a8875bb6f2
Cairo Pereira <cairo.151295@gmail.com>
2020-04-22 23:30:12 -0300

    Remover reason setado a fogo na classe

--- ws/src/main/java/br/com/slogcorp/ws/rest/exception/AuthenticationException.java
+++ ws/src/main/java/br/com/slogcorp/ws/rest/exception/AuthenticationException.java
@@ -3,7 +3,7 @@ package br.com.slogcorp.ws.rest.exception;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ResponseStatus;
 
-@ResponseStatus(value = HttpStatus.UNAUTHORIZED, reason = "Login ou senha incorretos!")
+@ResponseStatus(value = HttpStatus.UNAUTHORIZED)
 public class AuthenticationException extends IllegalArgumentException {
 
     public AuthenticationException(String message) {
