07f0385450bf40c16f9e39676c5c5775c16b3e4b
Cairo Pereira <cairo.151295@gmail.com>
2020-04-22 23:36:33 -0300

    Adicionar metodo de buscar por e-mail

--- ws/src/main/java/br/com/slogcorp/ws/rest/repository/UserRepository.java
+++ ws/src/main/java/br/com/slogcorp/ws/rest/repository/UserRepository.java
@@ -7,5 +7,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import java.util.Optional;
 
 public interface UserRepository extends JpaRepository<User, Integer> {
-    Optional<User> findByLoginAndPassword(String login, String password);
+    Optional<User> findByEmailAndPassword(String login, String password);
+
+    Optional<User> findByEmail(String email);
 }
