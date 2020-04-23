4de5686dc401e299bbfd7b8206102b14d830e615
Cairo Pereira <cairo.151295@gmail.com>
2020-04-22 23:39:00 -0300

    Criar metodo de validação de usuario e ajustar metodo de busca de usuario.

--- ws/src/main/java/br/com/slogcorp/ws/rest/service/UserService.java
+++ ws/src/main/java/br/com/slogcorp/ws/rest/service/UserService.java
@@ -7,5 +7,5 @@ import java.util.Optional;
 public interface UserService {
     void create(User user);
 
-    Optional<User> findForLoginAndPassword(String login, String password);
+    Optional<User> findByEmailAndPassword(String login, String password);
 }
--- ws/src/main/java/br/com/slogcorp/ws/rest/service/impl/UserServiceImpl.java
+++ ws/src/main/java/br/com/slogcorp/ws/rest/service/impl/UserServiceImpl.java
@@ -1,5 +1,6 @@
 package br.com.slogcorp.ws.rest.service.impl;
 
+import br.com.slogcorp.ws.rest.exception.UserException;
 import br.com.slogcorp.ws.rest.model.User;
 import br.com.slogcorp.ws.rest.repository.UserRepository;
 import br.com.slogcorp.ws.rest.service.AdressService;
@@ -9,6 +10,9 @@ import org.springframework.stereotype.Service;
 import java.security.MessageDigest;
 import java.security.NoSuchAlgorithmException;
 import java.util.Optional;
+
+import static br.com.slogcorp.ws.rest.util.DocumentUtils.isValidCPF;
+import static br.com.slogcorp.ws.rest.util.EmailUtils.isValidEmailAddress;
 
 @Service
 public class UserServiceImpl implements UserService {
@@ -24,14 +28,33 @@ public class UserServiceImpl implements UserService {
 
     @Override
     public void create(User user) {
+        isValidUser(user);
         user.setAdress(adressService.findOrSave(user.getAdress()));
         user.setPassword(getEncryptoPassword(user.getPassword()));
         userRepository.save(user);
     }
 
+    private void isValidUser(User user) {
+
+        if (!isValidCPF(user.getDocument().toString())) {
+            throw new UserException("CPF Inválido!");
+        }
+        if (!isValidEmailAddress(user.getEmail())) {
+            throw new UserException("E-mail inválido!");
+        }
+        if(!emailExists(user.getEmail())){
+            throw new UserException("Este e-mail já esta sendo utilizado por outra conta.");
+        }
+    }
+
+    private boolean emailExists(String email){
+        Optional<User> userTemp = userRepository.findByEmail(email);
+        return userTemp.isPresent();
+    }
+
     @Override
-    public Optional<User> findForLoginAndPassword(String login, String password) {
-        return userRepository.findByLoginAndPassword(login, getEncryptoPassword(password));
+    public Optional<User> findByEmailAndPassword(String email, String password) {
+        return userRepository.findByEmailAndPassword(email, getEncryptoPassword(password));
     }
 
     private String getEncryptoPassword(String password) {
