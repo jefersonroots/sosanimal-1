5f3002576f79ee22864f2c60f6ad999c7293c7e9
Cairo Pereira <cairo.151295@gmail.com>
2020-04-23 01:08:00 -0300

    Criar metodo de buscar dados do endereço a partir do CEP.

--- ws/src/main/java/br/com/slogcorp/ws/rest/service/impl/AdressServiceImpl.java
+++ ws/src/main/java/br/com/slogcorp/ws/rest/service/impl/AdressServiceImpl.java
@@ -1,9 +1,19 @@
 package br.com.slogcorp.ws.rest.service.impl;
 
+import br.com.slogcorp.ws.rest.dto.AdressDTO;
+import br.com.slogcorp.ws.rest.exception.AdressException;
 import br.com.slogcorp.ws.rest.model.Adress;
 import br.com.slogcorp.ws.rest.repository.AdressRepository;
 import br.com.slogcorp.ws.rest.service.AdressService;
 import org.springframework.stereotype.Service;
+import org.springframework.web.client.HttpClientErrorException;
+import org.springframework.web.client.RestTemplate;
+import org.springframework.web.util.UriComponents;
+import org.springframework.web.util.UriComponentsBuilder;
+
+import java.util.Optional;
+
+import static br.com.slogcorp.ws.rest.util.AdressUtils.parseCepForInteger;
 
 @Service
 public class AdressServiceImpl implements AdressService {
@@ -22,5 +32,34 @@ public class AdressServiceImpl implements AdressService {
         if (newAdress == null) newAdress = adressRepository.save(adress);
 
         return newAdress;
+    }
+
+    @Override
+    public Adress findByCep(String cep) {
+        RestTemplate template = new RestTemplate();
+
+        UriComponents uri = UriComponentsBuilder.newInstance()
+                .scheme("http")
+                .host("viacep.com.br")
+                .path("ws/")
+                .path(cep.concat("/"))
+                .path("json/")
+                .build();
+
+        try {
+            Optional<AdressDTO> adressDTO = Optional.ofNullable(template.getForObject(uri.toUriString(), AdressDTO.class));
+            Adress adress = new Adress();
+            if (adressDTO.isPresent()) {
+                adress.setCep(parseCepForInteger(adressDTO.get().getCep()));
+                adress.setCity(adressDTO.get().getLocalidade());
+                adress.setStreet(adressDTO.get().getLogradouro());
+                adress.setDistrict(adressDTO.get().getBairro());
+                return adress;
+            }else{
+                throw new AdressException("Ocorreu um erro ao buscar os dados referente ao CEP [".concat(cep).concat("]"));
+            }
+        } catch (HttpClientErrorException ex) {
+            throw new AdressException("CEP Inválido!");
+        }
     }
 }
