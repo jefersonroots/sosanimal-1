d342d8574d79cca78b22800142aa8a44be706321
Jeferson <jefersonsaaantos@gmail.com>
2020-04-23 21:02:48 -0300

    configurar rotas

--- mobile/src/routes.js
+++ mobile/src/routes.js
@@ -1,24 +1,21 @@
 import React from 'react';
-import { createStackNavigator } from '@react-navigation/stack';
-import { NavigationContainer} from '@react-navigation/native';
+import {createStackNavigator} from '@react-navigation/stack';
+import {NavigationContainer} from '@react-navigation/native';
+
 import Auth from './pages/Auth';
-import Register from './pages/Register'; 
+import Register from './pages/Register';
 
 const AppStack = createStackNavigator();
-
-
 export default function Routes(){
     return(
         <NavigationContainer>
-            <AppStack.Navigator  screenOptions={{ headerShown:false}} >
-                <AppStack.Screen  name="LOGIN" component ={Auth} />
-            </AppStack.Navigator>
-       
-            <AppStack.Navigator  screenOptions={{ headerShown:false}} >
-                <AppStack.Screen  name="Register" component ={Register} />
-            </AppStack.Navigator>
-       
-      </NavigationContainer>
+
+        <AppStack.Navigator screenOptions={{}}>
+            <AppStack.Screen name="SOS ANIMAL " component={Auth}/>
+            <AppStack.Screen name="Register" component={Register}/>
+        </AppStack.Navigator>
+
+        </NavigationContainer>
     );
 }
  
