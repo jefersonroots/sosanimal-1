e9810ad35ba2e0a6147ee409b2756bf6564d5347
Pedro Viado <pedrohenrique9592@gmail.com>
2020-04-29 13:27:16 -0300

    Alterar tipo de teclado para number-pad

--- mobile/src/pages/RegisterOng/index.js
+++ mobile/src/pages/RegisterOng/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { View, Text, TextInput, TouchableOpacity, ScrollView } from 'react-native';
 import { Form } from '@unform/mobile';
+import { Scope } from '@unform/core';
 import styles from "./styles";
 
 
@@ -13,7 +14,6 @@ export default function Register() {
             <ScrollView showsVerticalScrollIndicator={false}>
                 <Text style={styles.title}>Cadastrar ONG</Text>
 
-                <View>
                     <Form>
                         <TextInput style={styles.input}
                             placeholder="Nome"
@@ -28,30 +28,29 @@ export default function Register() {
                             autoCorrect={false}
                         />
                         <TextInput style={styles.input}
-                            placeholder="Telefone"
-                            autoCorrect={false}
-                        />
-                        <TextInput style={styles.input}
                             placeholder="CNPJ"
                             autoCorrect={false}
+                            keyboardType="number-pad"
                         />
                         <TextInput style={styles.input}
                             placeholder="Telefone 1"
                             autoCorrect={false}
+                            keyboardType="number-pad"
                         />
                         <TextInput style={styles.input}
                             placeholder="Telefone 2"
                             autoCorrect={false}
+                            keyboardType="number-pad"
                         />
 
                         <View>
                             <Text style={styles.subtitle}>Endereço</Text>
-
-                            <View style={styles.formEndereco}>
-                                <Form>
+                            <Scope path="address">
+                                <View style={styles.formEndereco}>
                                     <TextInput style={styles.inputCep}
                                         placeholder="CEP"
                                         autoCorrect={false}
+                                        keyboardType="number-pad"
                                     />
                                     <TouchableOpacity
                                         style={styles.botaoVerificar}
@@ -59,49 +58,47 @@ export default function Register() {
                                     >
                                         <Text style={styles.botaoText}>Verificar</Text>
                                     </TouchableOpacity>
-                                </Form>
-                            </View>
+                                </View>
 
-                            <View style={styles.formEndereco}>
-                                <TextInput style={styles.inputCidade}
+                                <View style={styles.formEndereco}>
+                                    <TextInput style={styles.inputCidade}
+                                        editable={false}
+                                        placeholder="Cidade"
+                                        autoCorrect={false}
+                                    />
+                                    <TextInput style={styles.inputUf}
+                                        editable={false}
+                                        placeholder="UF"
+                                        autoCorrect={false}
+                                    />
+                                </View>
+
+                                <TextInput style={styles.inputBairro}
                                     editable={false}
-                                    placeholder="Cidade"
+                                    placeholder="Bairro"
                                     autoCorrect={false}
                                 />
-                                <TextInput style={styles.inputUf}
-                                    editable={false}
-                                    placeholder="UF"
-                                    autoCorrect={false}
-                                />
-                            </View>
 
-                            <TextInput style={styles.inputBairro}
-                                editable={false}
-                                placeholder="Bairro"
-                                autoCorrect={false}
-                            />
-
-                            <View style={styles.formEndereco}>
-                                <TextInput style={styles.inputComplemento}
-                                    placeholder="Complemento"
-                                    autoCorrect={false}
-                                />
-                                <TextInput style={styles.inputNumero}
-                                    placeholder="Número"
-                                    autoCorrect={false}
-                                />
-                            </View>
-
+                                <View style={styles.formEndereco}>
+                                    <TextInput style={styles.inputComplemento}
+                                        placeholder="Complemento"
+                                        autoCorrect={false}
+                                    />
+                                    <TextInput style={styles.inputNumero}
+                                        placeholder="Número"
+                                        autoCorrect={false}
+                                        keyboardType="number-pad"
+                                    />
+                                </View>
+                            </Scope>
                             <TouchableOpacity
                                 style={styles.botao}
                                 onPress={() => { }}
                             >
                                 <Text style={styles.botaoText}>Cadastrar</Text>
                             </TouchableOpacity>
-
                         </View>
                     </Form>
-                </View>
             </ScrollView>
         </View>
     );
